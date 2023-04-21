05fb840c749d93e26b86c708b78dc7e6880f73c6
pankajkumar3021 <pankajkumar.3021@gmail.com>
2023-04-22 00:47:34 +0530

    new upddates

--- emacs/init.el
+++ emacs/init.el
@@ -124,4 +124,259 @@
 
 ;; undo
 (global-set-key (kbd "C-z") 'undo)
+;;extra
+
+;; key binding for directly going to orgmode file
+(global-set-key (kbd "C-c o") 
+(lambda () (interactive) (find-file "c:/pankaj/sw/emacs/.emacs.d/org/projects.org")))
+(global-set-key (kbd "C-c a") 'org-agenda)
+(define-key global-map "\C-cc" 'org-capture)
+(setq org-log-done t)
+(setq org-capture-templates
+      '(    
+        ("j" "Work Log Entry"
+         entry (file+datetree "c:/pankaj/sw/emacs/.emacs.d/org/work-log.org")
+         "* %?"
+         :empty-lines 0)
+
+        ("n" "Note"
+         entry (file+headline "c:/pankaj/sw/emacs/.emacs.d/org/notes.org" "Random Notes")
+         "** %?"
+         :empty-lines 0)
+
+        ("n" "Note"
+         entry (file+headline "c:/pankaj/sw/emacs/.emacs.d/org/notes.org" "Random Notes")
+         "** %?"
+         :empty-lines 0)
+
+        ("d" "Door Codes"
+         entry (file+headline "c:/pankaj/sw/emacs/.emacs.d/org/notes.org" "Door Codes")
+         "** %?"
+         :empty-lines 0)
+
+        ("g" "General To-Do"
+         entry (file+headline "c:/pankaj/sw/emacs/.emacs.d/org/todos.org" "General Tasks")
+         "* TODO [#B] %?\n:Created: %T\n "
+         :empty-lines 0)
+
+        ("m" "Meeting"
+         entry (file+datetree "c:/pankaj/sw/emacs/.emacs.d/org/meetings.org")
+         "* %? :meeting:%^g \n:Created: %T\n** Attendees\n*** \n** Notes\n** Action Items\n*** TODO [#A] "
+         :tree-type week
+         :clock-in t
+         :clock-resume t
+         :empty-lines 0)
+		 
+	("c" "Code To-Do"
+         entry (file+headline "c:/pankaj/sw/emacs/.emacs.d/org/todos.org" "Code Related Tasks")
+         "* TODO [#B] %?\n:Created: %T\n%i\n%a\nProposed Solution: "
+         :empty-lines 0)
+
+        )
+
+)
+
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "PLANNING(p)" "IN-PROGRESS(i@/!)" "VERIFYING(v!)" "BLOCKED(b@)"  "|" "DONE(d!)" "OBE(o@!)" "WONT-DO(w@/!)" )
+        ))
+
+(setq org-todo-keyword-faces
+      '(
+        ("TODO" . (:foreground "GoldenRod" :weight bold))
+        ("PLANNING" . (:foreground "DeepPink" :weight bold))
+        ("IN-PROGRESS" . (:foreground "Cyan" :weight bold))
+        ("VERIFYING" . (:foreground "DarkOrange" :weight bold))
+        ("BLOCKED" . (:foreground "Red" :weight bold))
+        ("DONE" . (:foreground "LimeGreen" :weight bold))
+        ("OBE" . (:foreground "LimeGreen" :weight bold))
+        ("WONT-DO" . (:foreground "LimeGreen" :weight bold))
+        ))
+
+
+;; Tags
+(setq org-tag-alist '(
+                      ;; Ticket types
+                      (:startgroup . nil)
+                      ("@bug" . ?b)
+                      ("@feature" . ?u)
+                      ("@spike" . ?j)                      
+                      (:endgroup . nil)
+
+                      ;; Ticket flags
+                      ("@write_future_ticket" . ?w)
+                      ("@emergency" . ?e)
+                      ("@research" . ?r)
+
+                      ;; Meeting types
+                      (:startgroup . nil)
+                      ("big_sprint_review" . ?i)
+                      ("cents_sprint_retro" . ?n)
+                      ("dsu" . ?d)
+                      ("grooming" . ?g)
+                      ("sprint_retro" . ?s)
+                      (:endgroup . nil)
+
+                      ;; Code TODOs tags
+                      ("QA" . ?q)
+                      ("backend" . ?k)
+                      ("broken_code" . ?c)
+                      ("frontend" . ?f)
+
+                      ;; Special tags
+                      ("CRITICAL" . ?x)
+                      ("obstacle" . ?o)
+                      
+                      ;; Meeting tags
+                      ("HR" . ?h)
+                      ("general" . ?l)
+                      ("meeting" . ?m)
+                      ("misc" . ?z)
+                      ("planning" . ?p)
+
+                      ;; Work Log Tags
+                      ("accomplishment" . ?a)
+                      ))
+
+
+;; Tag colors
+(setq org-tag-faces
+      '(
+        ("planning"  . (:foreground "mediumPurple1" :weight bold))
+        ("backend"   . (:foreground "royalblue1"    :weight bold))
+        ("frontend"  . (:foreground "forest green"  :weight bold))
+        ("QA"        . (:foreground "sienna"        :weight bold))
+        ("meeting"   . (:foreground "yellow1"       :weight bold))
+        ("CRITICAL"  . (:foreground "red1"          :weight bold))
+        )
+      )
+
+
+;; Agenda View "d"
+(defun air-org-skip-subtree-if-priority (priority)
+  "Skip an agenda subtree if it has a priority of PRIORITY.
+
+    PRIORITY may be one of the characters ?A, ?B, or ?C."
+  (let ((subtree-end (save-excursion (org-end-of-subtree t)))
+        (pri-value (* 1000 (- org-lowest-priority priority)))
+        (pri-current (org-get-priority (thing-at-point 'line t))))
+    (if (= pri-value pri-current)
+        subtree-end
+      nil)))
+
+(setq org-agenda-skip-deadline-if-done t)
+
+(setq org-agenda-custom-commands
+      '(
+
+        ;; Daily Agenda & TODOs
+        ("d" "Daily agenda and all TODOs"
+
+         ;; Display items with priority A
+         ((tags "PRIORITY=\"A\""
+                ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                 (org-agenda-overriding-header "High-priority unfinished tasks:")))
+
+          ;; View 7 days in the calendar view
+          (agenda "" ((org-agenda-span 7)))
+
+          ;; Display items with priority B (really it is view all items minus A & C)
+          (alltodo ""
+                   ((org-agenda-skip-function '(or (air-org-skip-subtree-if-priority ?A)
+                                                   (air-org-skip-subtree-if-priority ?C)
+                                                   (org-agenda-skip-if nil '(scheduled deadline))))
+                    (org-agenda-overriding-header "ALL normal priority tasks:")))
+
+          ;; Display items with pirority C
+          (tags "PRIORITY=\"C\""
+                ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                 (org-agenda-overriding-header "Low-priority Unfinished tasks:")))
+          )
+
+         ;; Don't compress things (change to suite your tastes)
+         ((org-agenda-compact-blocks nil)))
+        
+        ;; James's Super View
+        ("j" "James's Super View"
+         (
+          (agenda ""
+                  (
+                   (org-agenda-remove-tags t)                                       
+                   (org-agenda-span 7)
+                   )
+                  )
+
+          (alltodo ""
+                   (
+                    ;; Remove tags to make the view cleaner
+                    (org-agenda-remove-tags t)
+                    (org-agenda-prefix-format "  %t  %s")                    
+                    (org-agenda-overriding-header "CURRENT STATUS")
+
+                    ;; Define the super agenda groups (sorts by order)
+                    (org-super-agenda-groups
+                     '(
+                       ;; Filter where tag is CRITICAL
+                       (:name "Critical Tasks"
+                              :tag "CRITICAL"
+                              :order 0
+                              )
+                       ;; Filter where TODO state is IN-PROGRESS
+                       (:name "Currently Working"
+                              :todo "IN-PROGRESS"
+                              :order 1
+                              )
+                       ;; Filter where TODO state is PLANNING
+                       (:name "Planning Next Steps"
+                              :todo "PLANNING"
+                              :order 2
+                              )
+                       ;; Filter where TODO state is BLOCKED or where the tag is obstacle
+                       (:name "Problems & Blockers"
+                              :todo "BLOCKED"
+                              :tag "obstacle"                              
+                              :order 3
+                              )
+                       ;; Filter where tag is @write_future_ticket
+                       (:name "Tickets to Create"
+                              :tag "@write_future_ticket"
+                              :order 4
+                              )
+                       ;; Filter where tag is @research
+                       (:name "Research Required"
+                              :tag "@research"
+                              :order 7
+                              )
+                       ;; Filter where tag is meeting and priority is A (only want TODOs from meetings)
+                       (:name "Meeting Action Items"
+                              :and (:tag "meeting" :priority "A")
+                              :order 8
+                              )
+                       ;; Filter where state is TODO and the priority is A and the tag is not meeting
+                       (:name "Other Important Items"
+                              :and (:todo "TODO" :priority "A" :not (:tag "meeting"))
+                              :order 9
+                              )
+                       ;; Filter where state is TODO and priority is B
+                       (:name "General Backlog"
+                              :and (:todo "TODO" :priority "B")
+                              :order 10
+                              )
+                       ;; Filter where the priority is C or less (supports future lower priorities)
+                       (:name "Non Critical"
+                              :priority<= "C"
+                              :order 11
+                              )
+                       ;; Filter where TODO state is VERIFYING
+                       (:name "Currently Being Verified"
+                              :todo "VERIFYING"
+                              :order 20
+                              )
+                       )
+                     )
+                    )
+                   )
+          ))
+        ))
+;;setq default-directory "C:\\pankaj\sw\emacs\\.emacs.d\\"
+(setq default-directory "C:\pankaj\sw\emacs\.emacs.d")
 
